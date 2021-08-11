ac0707fec26fcbcc256b481271d83c729c6e139b
Henrique Guimaraes <henrique.guimaraes@dommus.com.br>
2021-08-11 13:59:05 -0300

    Arquivos base

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+import {createServer, Model} from 'miragejs';
+
+createServer({
+    models:{
+      transaction: Model,
+    },
+
+    seeds(server){
+      server.db.loadData({
+        transactions: [
+          {
+            id: 1,
+            title: 'Desenvolviemnto Front-end',
+            type: 'deposit',
+            category: 'Dev',
+            amount: 5000,
+            createdAt: new Date ('2021-02-12 09:00:00'),
+          },
+          {
+            id: 2,
+            title: 'Aluguel',
+            type: 'withdraw',
+            category: 'Escritório',
+            amount: 600,
+            createdAt: new Date ('2021-02-14 19:00:00'),
+          }
+        ],
+      })
+
+
+    },
+
+  routes(){
+    this.namespace =  'api';
+
+    this.get('/transactions', () =>{
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) =>{
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +54,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
